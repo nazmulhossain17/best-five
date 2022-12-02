282b3ea155b92da34f45b6337f662c3e9378ed1c
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2022-12-03 01:34:52 +0600

    app.js select.js

---
+++ js/select.js
@@ -0,0 +1,71 @@
+
+document.getElementById('select1').addEventListener('click', function(){
+    const playerName = document.getElementById('name1');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select2').addEventListener('click', function(){
+    const playerName = document.getElementById('name2');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list2');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select3').addEventListener('click', function(){
+    const playerName = document.getElementById('name3');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list3');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select4').addEventListener('click', function(){
+    const playerName = document.getElementById('name4');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list4');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select5').addEventListener('click', function(){
+    const playerName = document.getElementById('name5');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list5');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select6').addEventListener('click', function(){
+    const playerName = document.getElementById('name6');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list1');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select7').addEventListener('click', function(){
+    const playerName = document.getElementById('name7');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list2');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select8').addEventListener('click', function(){
+    const playerName = document.getElementById('name8');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list3');
+    list1.innerText = playerNameText;
+})
+document.getElementById('select9').addEventListener('click', function(){
+    const playerName = document.getElementById('name9');
+    const playerNameText = playerName.innerText;
+
+    const list1 = document.getElementById('list4');
+    list1.innerText = playerNameText;
+})
+
+
+
+
+
+
+
